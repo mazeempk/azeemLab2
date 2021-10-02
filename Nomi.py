5a67a0acd3bbdcca0f4cdb6bb6abf9e011f3b173
nouman47 <nouman2019@namal.edu.pk>
2021-10-03 01:58:32 +0500

    Nomi commits

---
+++ Nomi.py
@@ -0,0 +1,20 @@
+def add(x,y):
+	return x + y
+
+a = add(10, 12)
+print(a)
+
+def mul(n1,n2):
+	return n1*n2
+print(mul(4,2)) 
+
+
+
+#division
+
+num1 = ("enter the first number")
+num2 = ("enter the second number")
+
+div= num1/num2
+
+print("the division of {0} and {1} is: {2}" , format(num1 , num2 , div)) 
